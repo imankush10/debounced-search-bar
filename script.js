001a88fa0d425e777903da8146a6abf87cf1887e
Ankush Kumar <imankush1010@gmail.com>
2023-12-28 17:01:53 +0530

    YESS

---
+++ script.js
@@ -0,0 +1,55 @@
+const userCardTemplate = document.querySelector("[data-user-card]");
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+async function dataFetch(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  data.forEach((data) => {
+    const html = `<div class="card">
+            <div class="header" data-header="${data.name}">${data.name}</div>
+            <div class="body" data-body="${data.name}">${data.email}</div>
+            </div>`;
+    users.push({
+      name: data.name,
+      email: data.email,
+    });
+    document
+      .querySelector(".user-cards")
+      .insertAdjacentHTML("afterbegin", html);
+  });
+}
+
+dataFetch("https://jsonplaceholder.typicode.com/users");
+
+const debouncedFunctionCaller = debouncingFunction((value) => {
+  users.forEach((user) => {
+    const card = document.querySelector(
+      `.header[data-header="${user.name}"`
+    ).parentNode;
+
+    const isVisible =
+    user.name.toLowerCase().includes(value) ||
+    user.email.toLowerCase().includes(value);
+
+    card.classList.toggle("hidden", !isVisible);
+});
+}, 300);
+
+
+function debouncingFunction(callback, delay=500){
+    let timeout;
+    return function (value){
+        clearTimeout(timeout);
+        timeout = setTimeout(()=>{
+            console.log("Searches now");
+            callback(value)
+        }, delay)
+    }
+}
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  debouncedFunctionCaller(value);
+});
